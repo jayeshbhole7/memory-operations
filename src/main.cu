ac795207c37adc8885daf9b76ada00ee94989d38
jayeshbhole7 <jayubhole7@gmail.com>
2025-01-08 09:25:32 +0530

    gpu:fix

--- src/main.cu
+++ src/main.cu
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-int main{
+
+__global__ void memory_read_write(int *a,int *b,int n){
+}
+
+int main(){
 
     const int n =1024;
 
@@ -13,16 +17,22 @@ int main{
 
     int *d_a,*d_b;
 
-    cudaMalloc(&d_a, n*sizeof(int));
-    cudaMalloc(&d_b, n*sizeof(int));
+    if(cudaMalloc(&d_a,n*sizeof(int))!=cudaSuccess){
+        cerr<<"cuda malloc fails for d_a\n";
+        return -1;
+    }
+    if(cudaMalloc(&d_a,n*sizeof(int))!=cudaSuccess){
+        cerr<<"cuda malloc fails for d_b\n";
+        return -1;
+    }
 
-    cudaMemcpy(d_a,a,n*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(d_a,a,n*sizeof(int), cudaMemcpyHostToDevice);
     
-    memory_read_write<<n/256,256>>(d_a,d_b,n);
+    memory_read_write<<<n/256,256>>>(d_a,d_b,n);
 
     cudaMemcpy(b,d_b,n*sizeof(int), cudaMemcpyDeviceToHost);
 
-    for(int i=0;i<10;i++) cout<<b[i]<<"";
+    for(int i=0;i<n;i++) cout<<b[i]<<" ";
 
     cudaFree(d_a);
     cudaFree(d_b);
